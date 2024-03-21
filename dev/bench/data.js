13b90df0d51e4ecbfbe0457d7bf979e0cba343b5
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 14:57:06 +0000

    add My Project Go Benchmark (catch2) benchmark result for 67ba804830cead1c99f6e7ad5f9621a559fd74de

---
+++ dev/bench/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711033026608,
+  "repoUrl": "https://github.com/wwerkk/dummyjuce",
+  "entries": {
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "W.K. Werkowicz",
+            "email": "wwerkowicz@gmail.com"
+          },
+          "committer": {
+            "name": "W.K. Werkowicz",
+            "email": "wwerkowicz@gmail.com"
+          },
+          "id": "67ba804830cead1c99f6e7ad5f9621a559fd74de",
+          "message": "add manual dispatch to gh-pages action",
+          "timestamp": "2024-03-21T14:56:41Z",
+          "url": "https://github.com/wwerkk/dummyjuce/commit/67ba804830cead1c99f6e7ad5f9621a559fd74de"
+        },
+        "date": 1711033026044,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Processor constructor",
+            "value": 15.3384,
+            "range": "± 5.4318",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Processor destructor",
+            "value": 1.74417,
+            "range": "± 641.04",
+            "unit": "us",
+            "extra": "100 samples\n42 iterations"
+          },
+          {
+            "name": "Editor open and close",
+            "value": 1.70501,
+            "range": "± 192.657",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
