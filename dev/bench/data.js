36dd2b2164c5799c245d4f3a300b4ca4db51f976
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 15:12:54 +0000

    add My Project Go Benchmark (catch2) benchmark result for 7ccfbfb413dd0a448f443176d4ab9c540a061f27

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711033838721,
+  "lastUpdate": 1711033974012,
   "repoUrl": "https://github.com/wwerkk/dummyjuce",
   "entries": {
     "Dummy JUCE Project Benchmark": [
@@ -64,6 +64,49 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/wwerkk/dummyjuce/commit/786f88b5a4b22424f6f65c70d60baa461865318b"
         },
         "date": 1711033836987,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Processor constructor",
+            "value": 15.3384,
+            "range": "± 5.4318",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Processor destructor",
+            "value": 1.74417,
+            "range": "± 641.04",
+            "unit": "us",
+            "extra": "100 samples\n42 iterations"
+          },
+          {
+            "name": "Editor open and close",
+            "value": 1.70501,
+            "range": "± 192.657",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "committer": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "distinct": true,
+          "id": "7ccfbfb413dd0a448f443176d4ab9c540a061f27",
+          "message": "change branch from master to main",
+          "timestamp": "2024-03-21T16:04:14+01:00",
+          "tree_id": "f65a07cf896be87f2eda2ea2bddf9ae6431020d3",
+          "url": "https://github.com/wwerkk/dummyjuce/commit/7ccfbfb413dd0a448f443176d4ab9c540a061f27"
+        },
+        "date": 1711033971007,
         "tool": "catch2",
         "benches": [
           {
