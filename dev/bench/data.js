e543ad20964251fc61999914c72e1568c948c9ba
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 15:22:22 +0000

    add Dummy JUCE Project Benchmark (catch2) benchmark result for 10b3858d0327fc2d1a1f23af49408f68a2d57092

---
+++ dev/bench/data.js
@@ -0,0 +1,51 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711034542321,
+  "repoUrl": "https://github.com/wwerkk/dummyjuce",
+  "entries": {
+    "Dummy JUCE Project Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "committer": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "distinct": true,
+          "id": "10b3858d0327fc2d1a1f23af49408f68a2d57092",
+          "message": "change project name in gh-pages workflow",
+          "timestamp": "2024-03-21T16:16:25+01:00",
+          "tree_id": "e4ad394d8b9747880f56ff96502215c7853d7e22",
+          "url": "https://github.com/wwerkk/dummyjuce/commit/10b3858d0327fc2d1a1f23af49408f68a2d57092"
+        },
+        "date": 1711034540871,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Processor constructor",
+            "value": 15.3384,
+            "range": "± 5.4318",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Processor destructor",
+            "value": 1.74417,
+            "range": "± 641.04",
+            "unit": "us",
+            "extra": "100 samples\n42 iterations"
+          },
+          {
+            "name": "Editor open and close",
+            "value": 1.70501,
+            "range": "± 192.657",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
