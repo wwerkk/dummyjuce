d31720fb23c2f679702042505af00685c28d6292
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 15:10:38 +0000

    add My Project Go Benchmark (catch2) benchmark result for 786f88b5a4b22424f6f65c70d60baa461865318b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711033026608,
+  "lastUpdate": 1711033838721,
   "repoUrl": "https://github.com/wwerkk/dummyjuce",
   "entries": {
     "Dummy JUCE Project Benchmark": [
@@ -19,6 +19,51 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/wwerkk/dummyjuce/commit/67ba804830cead1c99f6e7ad5f9621a559fd74de"
         },
         "date": 1711033026044,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Processor constructor",
+            "value": 15.3384,
+            "range": "± 5.4318",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
+          },
+          {
+            "name": "Processor destructor",
+            "value": 1.74417,
+            "range": "± 641.04",
+            "unit": "us",
+            "extra": "100 samples\n42 iterations"
+          },
+          {
+            "name": "Editor open and close",
+            "value": 1.70501,
+            "range": "± 192.657",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      }
+    ],
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "committer": {
+            "email": "wwerkowicz@gmail.com",
+            "name": "W.K. Werkowicz"
+          },
+          "distinct": true,
+          "id": "786f88b5a4b22424f6f65c70d60baa461865318b",
+          "message": "change branch name to main, comment out trigger",
+          "timestamp": "2024-03-21T16:04:56+01:00",
+          "tree_id": "b5a87c6e89c9435c93771c2517f30bfc7e0ebec7",
+          "url": "https://github.com/wwerkk/dummyjuce/commit/786f88b5a4b22424f6f65c70d60baa461865318b"
+        },
+        "date": 1711033836987,
         "tool": "catch2",
         "benches": [
           {
